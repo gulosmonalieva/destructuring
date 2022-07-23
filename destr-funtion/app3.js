1fcb695a2e0c4203d8c01ed276696fb4e07cd2d6
Gulshan Osmonalieva <gulshan.osmonalieva.kk@gmail.com>
2022-07-24 01:12:08 +0600

    finished

---
+++ destr-funtion/app3.js
@@ -0,0 +1,8 @@
+func(["Иван", "Иванов", "отдел разработки"]);
+
+function func([name, surname, department, position = "джуниор"]) {
+  console.log(name);
+  console.log(surname);
+  console.log(department);
+  console.log(position);
+}
---
+++ destr-funtion/app4.js
@@ -0,0 +1,12 @@
+func("отдел разработки", ["Иван", "Иванов"], [2018, 12, 31]);
+
+function func(deparment, [name, surname], [year, month, day]) {
+  console.log(deparment);
+
+  console.log(name);
+  console.log(surname);
+
+  console.log(year);
+  console.log(month);
+  console.log(day);
+}
